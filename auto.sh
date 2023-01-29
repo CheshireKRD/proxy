0bd69d6b7f376dda88002eed9f615defd66e914f
Pavel Gulyga <93519265+CheshireKRD@users.noreply.github.com>
2023-01-29 13:51:42 +0300

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,1 +1,1 @@
-yum -y update ; yum -y install squid ; yum -y install nano ; systemctl start squid ; systemctl enable squid ; cd /etc/squid ; rm -f squid.conf ; curl -o squid.conf https://github.com/CheshireKRD/proxy/blob/main/squid.conf ; systemctl restart squid
+yum -y update ; yum -y install squid ; yum -y install nano ; systemctl start squid ; systemctl enable squid ; cd /etc/squid ; rm -f squid.conf ; curl -o squid.conf https://raw.githubusercontent.com/CheshireKRD/proxy/main/squid.conf ; systemctl restart squid
